8d5cd3180319ea2025287996b27ef5902b5dcd75
Amy Ventura <amyvntr@yahoo.com>
2020-12-16 15:50:43 -0500

    added save record and error function

--- Develop/public/indexDB.js
+++ Develop/public/indexDB.js
@@ -21,11 +21,15 @@ request.onsuccess = ({ target }) => {
 // what happens if not online
 request.onerror = function(event) {
     // show an error in console => console.log(err)
+    console.log("Error " + event.target.errorCode);
 };
 
 // save to db (called if offline - request fail)
 function saveRecord(record) {
     // just saving a record
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
 };
 
 // back online, send to mongoDB and clear pending IndexDB 
@@ -35,7 +39,19 @@ function checkDatabase(){
     // use const getE.onsuccess = function (){ fetch => .then (return trans in json) .then (delete the records if successful (store.clear))}
     const transaction = db.transaction(["pending"], "readwrite");
     const store = transaction.objectStore("pending");
-    store.clear();
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function(){
+        if(getAll.result.length >0){
+            fetch("api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result), 
+                headers: {
+
+                }
+            })
+        }
+    }
 };
 
 // listen for app coming back online
