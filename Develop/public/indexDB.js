cbe2efcbbf6d97c36b03a0729b76d3b662ebd846
Amy Ventura <amyvntr@yahoo.com>
2020-12-16 15:43:32 -0500

    added to indexDB.js

--- Develop/public/indexDB.js
+++ Develop/public/indexDB.js
@@ -11,9 +11,11 @@ request.onupgradeneeded = ({ target }) => {
 
 // if not online check DB
 request.onsuccess = ({ target }) => {
-    // db = target.request
+    let db = target.request
     // check if app is online before reading db
-
+    if (navigator.online){
+        checkDatabase();
+    }
 };
 
 // what happens if not online
@@ -31,6 +33,9 @@ function checkDatabase(){
     // make trans and store(key word) variables
     // add a const(getE) for the store.getAll method
     // use const getE.onsuccess = function (){ fetch => .then (return trans in json) .then (delete the records if successful (store.clear))}
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.clear();
 };
 
 // listen for app coming back online
